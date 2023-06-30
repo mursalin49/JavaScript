055882c2407338c3f6e3edd2a3a8acd454fac964
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2023-06-30 10:38:17 +0600

    Add files via upload

---
+++ Assignment Operators.js
@@ -0,0 +1,16 @@
+var x = 5;
+var y = 2;
+var z= 10;
+ var a= x += y;
+ var b= x -= y;
+ var c= x *= y;
+ var d= x /= y;
+ var e= x %= y;
+ var f= z **= y;
+
+ console.log(a);
+ console.log(b);
+ console.log(c);
+ console.log(d); 
+ console.log(e);
+ console.log(f);
