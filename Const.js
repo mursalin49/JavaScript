6a4064d09fcf4926ae7e2ea6022d6bcb6e56c49c
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2023-06-30 09:04:03 +0600

    Add files via upload

---
+++ Const.js
@@ -0,0 +1,11 @@
+// Create an object:
+const car = {type:"Fiat", model:"500", color:"white"};
+
+// Change a property:
+car.color = "red";
+
+// Add a property:
+car.owner = "Johnson";
+
+// Display the property:
+console.log("car owner is", car.owner);
