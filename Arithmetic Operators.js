b239ffb941fed46c0d35b5b2ff923cdf2e945afd
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2023-06-30 09:41:14 +0600

    Add files via upload

---
+++ Arithmetic Operators.js
@@ -0,0 +1,25 @@
+var x = 5;
+var a = Math.pow(x,2);
+// power is same  as Math.pow(x,2) = **
+let z = x ** 2;
+var y = 2;
+var b = x % y;
+var c = x + y;
+var d = x - y;
+var e = x*y;
+var f = x/y;
+ // increment
+x++;
+let g = x; 
+//decrement
+x--;
+var h = x;
+console.log(a);
+console.log(z);
+console.log(b);
+console.log(c);
+console.log(d); 
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
