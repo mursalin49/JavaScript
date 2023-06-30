e4eb2b13f53502b131ca632018e7c6f1623f7112
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2023-06-30 21:03:30 +0600

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,24 @@
+
+ /*
+ // program to add two numbers using a function
+// declaring a function
+function add(a, b) {
+    console.log(a + b);
+}
+
+// calling functions
+add(3,4);
+add(2,9); */
+function myFunc(theObject) {
+    theObject.make = "Toyota";
+  }
+  
+  const mycar = {
+    make: "Honda",
+    model: "Accord",
+    year: 1998,
+  };
+  
+  console.log(mycar.make); // "Honda"
+  myFunc(mycar);
+  console.log(mycar.make); // "Toyota"
