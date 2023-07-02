11604c4acc172c20eb6c6d88c1570fbd53ebbfed
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2023-07-02 23:43:20 +0600

    Add files via upload

---
+++ String Search.js
@@ -0,0 +1,10 @@
+//String indexOf()
+let text = "Please locate where 'locate' occurs!";
+console.log(text.indexOf("locate"));
+//String lastIndexOf()
+/*The lastIndexOf() method returns the index of the last occurrence of a specified text in a string: */
+ text = "Please locate where 'locate' occurs!";
+console.log(text.lastIndexOf("locate"));
+//String search()
+ text = "Please locate where 'locate' occurs!";
+console.log(text.search("locate"));
