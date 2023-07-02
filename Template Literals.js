e3d80c132fce892b67d0cd0547cea8cd47da098a
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2023-07-02 23:53:31 +0600

    Add files via upload

---
+++ Template Literals.js
@@ -0,0 +1,6 @@
+// `` template literals
+let country = `Bangladesh`;
+let a =17;
+let b= 1;
+let sentence=` i love ${country} and it is a big country. it's population is ${a+b} crore`;
+console.log(sentence);
