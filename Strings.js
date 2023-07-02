705761056b6a9328715b9b3acebf38821137d923
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2023-07-02 20:49:54 +0600

    Add files via upload

---
+++ Strings.js
@@ -0,0 +1,7 @@
+let text = 'ABCDE "JKLMNO" PQRSTUVWXYZ';
+console.log(text.length);
+console.log(text);
+// When using the == operator, x and y are equal
+let x = "John";
+let y = new String("John");
+console.log(x==y);
