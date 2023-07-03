a78312ad8eafebb786caf38d4d8cf7cf0ca7ba80
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2023-07-03 10:05:19 +0600

    Add files via upload

---
+++ Numbers.js
@@ -0,0 +1,8 @@
+//Numbers can be written with or without decimals
+let x = 3.14;
+let y = 3;
+console.log(x);
+console.log(y);
+//Floating point arithmetic is not always 100% accurate
+ x = 0.2 + 0.1;
+ console.log(x);
