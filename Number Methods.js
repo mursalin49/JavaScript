1a697dc03d8f0dbb081f1ba558ec0f6ad482e32d
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2023-07-03 10:05:35 +0600

    Add files via upload

---
+++ Number Methods.js
@@ -0,0 +1,15 @@
+//toString() Method
+//The toString() method returns a number as a string.
+let x = 123;
+let newN = x.toString();
+console.log(typeof newN);
+//toExponential() Method
+//toExponential() returns a string, with a number rounded and written using exponential notation.
+ x = 9.656;
+x.toExponential(2);
+x.toExponential(4);
+x.toExponential(6);
+console.log(x.toExponential(2));
+console.log(x.toExponential(4));
+console.log(x.toExponential(6));
+//toFixed() Method
