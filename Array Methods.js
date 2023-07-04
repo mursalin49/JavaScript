4d55df42a1606cf40ba829f484eb2f320fe76a4f
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2023-07-04 08:19:09 +0600

    Add files via upload

---
+++ Array Methods.js
@@ -0,0 +1,11 @@
+//Array length
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.length);
+//Array toString()
+console.log(fruits.toString());
+//Array pop()
+//The pop() method removes the last element from an array
+console.log(fruits.pop());
+//Array push()
+//The push() method adds a new element to an array (at the end)
+console.log(fruits.push("Kiwi"));
